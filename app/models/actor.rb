2f76d8e81dd034e8a14530c4999443d6d389882c
dynamic-daemon-637b <zl3087@columbia.edu>
2021-06-25 02:40:02 +0000

    do

--- app/models/actor.rb
+++ app/models/actor.rb
@@ -5,7 +5,7 @@ def full_name
    "#{self.first_name} #{self.last_name}"
   end
 def list_roles
-characters.map {|character| "#{character.name} - #{character.show.name}"
-end
+characters.map {|character| 
+"#{character.name} - #{character.show.name}"}
 end
 end
--- db/migrate/006_add_day_genre_season_to_season.rb
+++ db/migrate/006_add_day_genre_season_to_season.rb
@@ -3,6 +3,6 @@ class AddCatchphraseToCharacter < ActiveRecord::Migration[5.1]
   #a new migration, and then in the change method,
   #we make the change.
   def change
-    add_column :character, :catchphrase, :integer
+    add_column :characters, :catchphrase, :string
   end
 end
